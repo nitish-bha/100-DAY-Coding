946d22f8736c33aaa7e501c59d6ad320ef3e4181
Nithish <nithishbharadwaj.akula@gmail.com>
2024-10-04 21:43:04 -0400

    commit day-01

---
+++ Linear Regression.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+df = pd.read_excel(r'C:\Users\Admin\Downloads\Heavenly Chocolate.xlsx')
+print(df)
+
+print(df.head())
+print(df.describe())
+print(df.columns)
+
